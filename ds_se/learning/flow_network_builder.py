329ad9836c0549f4e850568b73d90eb0602853bd
KingShark1 <manastiwari28@gmail.com>
2022-10-11 17:06:44 +0530

    add hrl network for task specific training

--- ds_se/learning/flow_network_builder.py
+++ ds_se/learning/flow_network_builder.py
@@ -2,11 +2,12 @@ from rl_games.algos_torch import torch_ext
 from rl_games.algos_torch import layers
 from rl_games.algos_torch import network_builder
 
-from learning import amp_agent
+from learning import amp_network_builder
 
 import torch
 import torch.nn as nn
 import numpy as np
+import enum
 
 DISC_LOGIT_INIT_SCALE = 1.0
 
@@ -16,7 +17,10 @@ class FLOWBuilder(network_builder.A2CBuilder):
 		return
 		
 	class Network(network_builder.A2CBuilder):
-		pass
+		def __init__(self, params, **kwargs):
+			super().__init__(params, **kwargs)
+			
+			
 
 	def build(self, name, **kwargs):
 		net = FLOWBuilder.Network(self.params, **kwargs)
